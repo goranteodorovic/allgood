f38c334df4f14e93373e7ef1268927d7abd1f7e7
Goran Teodorovic <fb.goran@gmail.com>
2020-10-31 12:47:48 +0100

    Header => Hero component, data const etc.

---
+++ src/app/data.const.ts
@@ -0,0 +1,21 @@
+import { faHandHoldingUsd } from '@fortawesome/free-solid-svg-icons';
+import { faMobile } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+export const HERO_LIST = [
+    {
+        icon: faHandHoldingUsd,
+        text: "Turn your stuff into cach donations",
+        color: "green"
+    },
+    {
+        icon: faMobile,
+        text: "Shop allgood market - and support charity",
+        color: "purple"
+    },
+    {
+        icon: faHeart,
+        text: "A new way to fund raise",
+        color: "red"
+    }
+]
--- src/app/header/hero/hero.component.ts
+++ src/app/header/hero/hero.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SharedService } from 'src/app/services/shared.service';
+import { HERO_LIST } from '../../data.const';
+
 
 @Component({
   selector: 'app-hero',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent implements OnInit {
+  image: string;
+  subscription: Subscription;
+  heroList = HERO_LIST
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: SharedService) {
+    this.subscription = this.screenResolutionSub()
   }
 
+  ngOnInit(): void {
+    this.subscription = this.screenResolutionSub()
+  }
+
+  screenResolutionSub() {
+    return this.service.screenResolution.subscribe((val: number) => {
+      this.image = val < 768 ? "../../../assets/media/hero-mobile.png" : "../../../assets/media/hero-main.png";
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
